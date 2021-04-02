505111b129519e647793ac16c869c8261fa56be0
Yoshidda <70910101+Yoshidda@users.noreply.github.com>
2021-04-02 02:00:26 +0100

    styling changes

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -3,150 +3,158 @@ import SearchBar from "../SearchBar/SearchBar";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import "../../App.css";
 
-
 const Weather = () => {
+  const [query, setQuery] = useState("");
 
-  const [query, setQuery] = useState("")
+  const [weather, setWeather] = useState({});
 
-  const [weather, setWeather] = useState({})
+  const [icon, setIcon] = useState("");
 
-  const [icon, setIcon] = useState("")
+  const [error, setError] = useState(false);
 
-  const [error, setError] = useState(false)
+  const [notFound, setNotFound] = useState(false);
 
-  const [notFound, setNotFound] = useState(false)
-
-
-  const getSearchInput = useRef()
+  const getSearchInput = useRef();
 
   const dateGenerator = (d) => {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
-      "November", "December"];
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
 
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-    const day = days[d.getDay()]
+    const day = days[d.getDay()];
     const date = d.getDate();
     const month = months[d.getMonth()];
-    const year = d.getFullYear()
+    const year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
-  }
+    return `${day} ${date} ${month} ${year}`;
+  };
 
   const getIcon = (weatherId) => {
     switch (true) {
       case weatherId >= 200 && weatherId <= 232:
-        setIcon("wu wu-white wu-128 wu-tstorm")
+        setIcon("wu wu-white wu-128 wu-tstorm");
         break;
       case weatherId >= 300 && weatherId <= 321:
-        setIcon("wu wu-white wu-128 wu-chancerain")
+        setIcon("wu wu-white wu-128 wu-chancerain");
         break;
       case weatherId >= 500 && weatherId <= 531:
-        setIcon("wu wu-white wu-128 wu-rain")
+        setIcon("wu wu-white wu-128 wu-rain");
         break;
       case weatherId >= 600 && weatherId <= 622:
-        setIcon("wu wu-white wu-128 wu-snow")
+        setIcon("wu wu-white wu-128 wu-snow");
         break;
       case weatherId >= 701 && weatherId <= 781:
-        setIcon("wu wu-white wu-128 wu-hazy")
+        setIcon("wu wu-white wu-128 wu-hazy");
         break;
       case weatherId === 800:
-        setIcon("wu wu-white wu-128 wu-clear")
+        setIcon("wu wu-white wu-128 wu-clear");
         break;
       case weatherId >= 801 && weatherId <= 804:
-        setIcon("wu wu-white wu-128 wu-cloudy")
+        setIcon("wu wu-white wu-128 wu-cloudy");
         break;
+
+      default:
+        return;
     }
-  }
+  };
 
-  const url = "https://api.openweathermap.org/data/2.5/"
-  const apiKey = "4277a7fa39b9695f4d6d2526640ef8dd"
-
+  const url = "https://api.openweathermap.org/data/2.5/";
+  const apiKey = "4277a7fa39b9695f4d6d2526640ef8dd";
 
   const fetchData = (e) => {
-
-
-    if (e.key == "Enter") {
+    if (e.key === "Enter") {
       if (query) {
         fetch(`${url}weather?q=${query}&units=metric&APPID=${apiKey}`)
-          .then(response => response.json())
+          .then((response) => response.json())
           .then((result) => {
             if (result.message === "city not found") {
-              setNotFound(true)
+              setNotFound(true);
               setTimeout(() => {
-                setNotFound(false)
-              }, 2000)
+                setNotFound(false);
+              }, 2000);
             } else {
-              setWeather(result)
-              getIcon(result.weather[0].id)
+              setWeather(result);
+              getIcon(result.weather[0].id);
             }
+          });
 
-          })
-
-        console.log(weather)
-        setQuery("")
+        setQuery("");
       } else {
-        setError(true)
+        setError(true);
 
         setTimeout(() => {
-          setError(false)
+          setError(false);
         }, 2000);
       }
 
-      getSearchInput.current.value = ""
-      console.log(query)
+      getSearchInput.current.value = "";
     }
+  };
 
-  }
-
-  const lazyLoading = weather < 0
-
-
-
+  const lazyLoading = weather < 0;
 
   return (
     <div>
-      {
-        lazyLoading ? "loading"
-          :
-          <div
-            className={
-              typeof weather.main != "undefined"
-                ? weather.main.temp > 18
-                  ? "warm"
-                  : "cool"
-                : "landing"
-            }
-          >
-            <SearchBar
-              query={query}
-              fetchData={fetchData}
-              setQuery={setQuery}
-              getSearchInput={getSearchInput}
-              notFound={notFound}
-              error={error}
+      {lazyLoading ? (
+        "loading"
+      ) : (
+        <div
+          className={
+            typeof weather.main != "undefined"
+              ? weather.main.temp > 18
+                ? "warm"
+                : "cool"
+              : "landing"
+          }
+        >
+          <SearchBar
+            query={query}
+            fetchData={fetchData}
+            setQuery={setQuery}
+            getSearchInput={getSearchInput}
+            notFound={notFound}
+            error={error}
+          />
+
+          {typeof weather.main !== "undefined" ? (
+            <WeatherCard
+              weather={weather.weather[0].description}
+              temperature={Math.floor(weather.main.temp)}
+              maxTemp={Math.floor(weather.main.temp_max)}
+              minTemp={Math.floor(weather.main.temp_min)}
+              city={weather.name}
+              country={weather.sys.country}
+              icon={icon}
+              date={dateGenerator(new Date())}
             />
-
-
-            {typeof weather.main !== "undefined" ?
-              <WeatherCard
-                weather={weather.weather[0].description}
-                temperature={weather.main.temp}
-                maxTemp={weather.main.temp_max}
-                minTemp={weather.main.temp_min}
-                city={weather.name}
-                country={weather.sys.country}
-                icon={icon}
-                date={dateGenerator(new Date())}
-              />
-              : <WeatherCard />}
-
-
-          </div>
-      }
+          ) : (
+            <WeatherCard />
+          )}
+        </div>
+      )}
     </div>
-
   );
-}
+};
 
 export default Weather;
